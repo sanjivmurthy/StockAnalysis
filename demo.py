369e46adecf0df21af68d6b1d9dcd579248a80b0
sanjiv murthy <sanjivp98@berkeley.edu>
2018-06-22 18:54:35 -0700

    both graphs next 2 each other

--- demo.py
+++ demo.py
@@ -27,9 +27,9 @@ app.layout =  html.Div([
     html.Div([html.H1('Live Market Sentiment Analysis', style = {'position' : 'relative' , 'top' : '15px'})]),
 
     html.Div([ html.H2('Crypto Price by the Minute', style = {'float' : 'left'} ),
-    html.Div([ html.H2('Stock Price by the Minute (US)', style = {'float' : 'left', 'margin-left':'28%'} ),
-    # # html.Div([ html.H3('bullshit space waster' , style = {'float' : 'left' , 'top' : '30px' })]),
-    # html.Div([ html.H3('Trading Hours: Monday-Friday 9:30:00 - 16:00:00 EST' , style = {'float' : 'left' ,'margin-left':'50%'})]),
+    html.Div([ html.H2('Stock Price by the Minute (US)', style = {'float' : 'left', 'margin-left':'50%'} ),
+    html.Div([ html.H3('bullshit space waster' , style = {'float' : 'left' })]),
+    html.Div([ html.H3('Trading Hours: Monday-Friday 9:30:00 - 16:00:00 EST' , style = {'float' : 'left' ,'margin-left':'50%'})]),
 
 
     dcc.RadioItems(
@@ -44,12 +44,7 @@ app.layout =  html.Div([
         ],
         value='BTC' , style = {'float': 'left' ,'width': '49%', 'top' : '150px'}
     )
-    ,dcc.Graph(id='my-graph2' , style = {'width': '49%', 'left': '0px' , 'position' : 'relative' , 'top' : '150px'})
-    ]),
-
-    html.Div([ html.H2('Stock Price by the Minute (US)' , style = {'position' : 'relative' ,'top' : '15px','left':'50%', 'margin-right':'0'}),
-    html.Div([ html.H3('Trading Hours: Monday-Friday 9:30:00 - 16:00:00 EST' , style = {'position' : 'relative' ,'left':'50%', 'margin-right':'0'})]),
-    dcc.RadioItems(
+    , dcc.RadioItems(
         id='stock',
         options=[
             {'label': 'Microsoft', 'value': 'MSFT'},
@@ -63,12 +58,34 @@ app.layout =  html.Div([
             {'label': 'Twitter', 'value': 'TWTR'},
             {'label': 'Nike', 'value': 'NKE'}
         ],
-        value='MSFT' , style = {'width': '49%', 'position' : 'relative' , 'left':'50%', 'margin-right':'0' }
+        value='MSFT' , style = {'float': 'right' , 'margin-right':'0','width': '49%', 'top' : '150px'}
     )
-    , dcc.Graph(id='my-graph', style = {'width': '49%' , 'position' : 'relative' , 'left':'50%', 'margin-right':'0' })
-    ]),
+    ,
+    dcc.Graph(id='my-graph2' , style = {'width': '49%','position' : 'relative' , 'float':'left', 'margin-right':'0'})
+    ]), dcc.Graph(id='my-graph', style = {'width': '49%','position' : 'relative' , 'float':'right', 'margin-left':'0' })
+
+    # html.Div([ html.H2('Stock Price by the Minute (US)' , style = {'position' : 'relative' ,'float' : 'left', 'top' : '15px','left':'50%', 'margin-right':'0'}),
+    # html.Div([ html.H3('Trading Hours: Monday-Friday 9:30:00 - 16:00:00 EST' , style = {'position' : 'relative' ,'left':'50%', 'margin-right':'0'})]),
+    # dcc.RadioItems(
+    #     id='stock',
+    #     options=[
+    #         {'label': 'Microsoft', 'value': 'MSFT'},
+    #         {'label': 'Tesla', 'value': 'TSLA'},
+    #         {'label': 'Apple', 'value': 'AAPL'},
+    #         # {'label': 'Berkshire Hathaway', 'value': 'BRK.A'},
+    #         {'label': 'Disney', 'value': 'DIS'},
+    #         # {'label': 'Facebook', 'value': 'FB'},
+    #         # {'label': 'Bank of America', 'value': 'BAC'},
+    #         {'label': 'Netflix', 'value': 'NFLX'},
+    #         {'label': 'Twitter', 'value': 'TWTR'},
+    #         {'label': 'Nike', 'value': 'NKE'}
+    #     ],
+    #     value='MSFT' , style = {'width': '49%', 'position' : 'relative' , 'float':'left', 'margin-right':'0' }
+    # )
+    # , dcc.Graph(id='my-graph', style = {'position' : 'relative' , 'float':'left', 'margin-right':'0' })
     ]),
     ])
+    # ])
 
 
 
